9185aa041e9c447abb2f7987dfdd03ac6b2a93b8
Rajat <30935718+rajatpal96@users.noreply.github.com>
2019-01-26 21:29:59 +0530

    delete installation file

--- src/main/resources/linux64/agent_start.sh
+++
@@ -1,3 +0,0 @@
-# script to download and install agent in shell
-cd /home/thehobbit/Desktop/agent
-java -jar -Dlog4j.configuration=file:"/home/thehobbit/Desktop/agent/log4j.properties" scrappy-1.0.jar /home/thehobbit/Desktop/agent/config.properties
